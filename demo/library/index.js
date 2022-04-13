5e2c6fb81039c19accc634e39a869781b2278dfc
Louis-Edouard <lelafontant@yahoo.ca>
2022-04-12 23:42:23 -0400

    docs updated: editor added

--- demo/library/index.js
+++ demo/library/index.js
@@ -2,34 +2,28 @@ const CONCEPT = require('@models/library-model/concept.json');
 const PROJECTION = require('@models/library-model/projection.json');
 const FIELD_PROJECTION = require('@models/library-model/field-projection.json');
 
-const HANDLER = {
-    "open-option": function (args) {
-        let concept = args[0];
-        let projection = this.createProjection(concept, "option");
-
-        let window = this.findWindow("side-instance");
-        if (!window) {
-            window = this.createWindow("side-instance");
-            window.container.classList.add("td-option-sideview");
-        }
-
-        if (window.instances.size > 0) {
-            let instance = Array.from(window.instances)[0];
-            instance.delete();
-        }
-
-        let instance = this.createInstance(concept, projection, {
-            type: "projection",
-            close: "DELETE-PROJECTION"
-        });
-
-        window.addInstance(instance);
-    },
-};
+const AUTHORS_DATA = require('./data/authors.json');
 
 let editor = Gentleman.activateEditor(".app-editor")[0];
 editor.init({
+    config: {
+        header: false,
+    },
     conceptModel: CONCEPT,
-    projectionModel: [PROJECTION, FIELD_PROJECTION],
-    handlers: HANDLER
+    projectionModel: [PROJECTION, FIELD_PROJECTION]
 });
+
+if (Array.isArray(AUTHORS_DATA)) {
+    AUTHORS_DATA.forEach(item => {
+        let author = editor.createConcept("author");
+
+        for (const key in item) {
+            const value = item[key];
+            if (author.hasAttribute(key)) {
+                author.getAttribute(key).setValue(value);
+            }
+        }
+    });
+}
+
+editor.createInstance("library").changeSize("fullscreen");
