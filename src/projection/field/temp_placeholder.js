7496022c32399c37f6d61c18371f32b45aaf1207
Aurelien Ducoin <aurelien210@numericable.fr>
2024-03-03 12:57:47 +0100

    feat(placeholder):refonte field

--- src/projection/field/temp_placeholder.js
+++ src/projection/field/temp_placeholder.js
@@ -5,14 +5,21 @@ import { Field } from "./field";
 
 const { isNullOrUndefined } = require("zenkai");
 
+function hasDimensions(item) {
+    return !isNullOrUndefined(item.getAttribute("width")) &&
+    !isNullOrUndefined(item.getAttribute("width")) &&
+    !isNullOrUndefined(item.getAttribute("x")) &&
+    !isNullOrUndefined(item.getAttribute("y"));
+}
+
 function getItem(element) {
-    const isValid = (el) => el.parentElement === this.choices;
+    const isValid = (el) => el.parentElement === this.element;
 
     if (isValid(element)) {
         return element;
     }
 
-    return findAncestor(element, isValid, 5);
+    return findAncestor(element, isValid, 10);
 }
 
 const isSame = (val1, val2) => {
@@ -53,73 +60,43 @@ function getItemValue(item) {
 
 
 const BasePlaceholderField = {
-    init(args){
-        this.placeholders = [];
+
+    init(args) {
+        Object.assign(this.schema, args);
+
+        const { focusable = false } = this.schema;
+
+        this.focusable = focusable;
 
         return this;
     },
 
-    render(){
-        const {} = this.schema;
+    render() {
 
-        if(isNullOrUndefined(this.element)){
+        if(isNullOrUndefined(this.element)) {
             this.element = document.createElementNS("http://www.w3.org/2000/svg", "svg");
             this.element.id = this.id;
             this.element.classList.add("field");
             this.element.tabIndex = -1;
 
             this.element.dataset.nature = "field";
-            this.element.dataset.view = "placeholder";
+            this.element.dataset.view = "svg-placeholder";
             this.element.dataset.id = this.id;
-        }
-
-        if(isNullOrUndefined(this.selectList)){
-            this.selectList = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-            this.element.append(this.selectList);
-        }
-
-        if(isNullOrUndefined(this.choices)){
-            this.choices = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-            this.selectList.append(this.choices);
+            this.element.dataset.ignore = "all";
         }
 
         this.values = this.source.getCandidates();
-        this.values.forEach(value => {
-            this.choices.append(this.createChoiceOption(value));
+
+        this.values.forEach( (value) => {
+            this.element.append(this.createChoiceOption(value));
         })
-
-        if(this.source.hasValue()){
-            this.setValue(this.source.getValue());
-        }
-
-        if(isNullOrUndefined(this.icon)){
-            this.createIcon();
-            this.element.append(this.selectListValue);
-        }
-
 
         this.bindEvents();
 
         return this.element;
     },
 
-    createIcon(){
-        this.selectListValue = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        const { content = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\"><rect width=\"17\" height=\"17\" rx=\"3\" x=\"1.5\" y=\"1.5\" stroke=\"#7c7d7c\" stroke-width=\"1.5\" fill=\"#fefefe\"></rect><path d=\"M 4 4 L 10 12 L 16 4 Z\" fill=\"#4a4a4a\"></path></svg>",
-        adaptable = false } = this.schema.icon;
-
-        const parser = new DOMParser();
-
-        this.icon = parser.parseFromString(content.replace(/\&nbsp;/g, ''), "image/svg+xml").documentElement;
-        this.icon.dataset.adaptable = adaptable;
-        this.icon.dataset.ignore = "all";
-        this.selectListValue.append(this.icon);
-
-        this.element.prepend(this.selectListValue);
-
-    },
-
-    createChoiceOption(value){
+    createChoiceOption(value) {
         const { template = {} } = this.schema.choice.option;
 
         const isConcept = isObject(value);
@@ -140,160 +117,155 @@ const BasePlaceholderField = {
             container.dataset.type = "meta-concept";
             container.dataset.value = value.name;
             container.append(render);
-            this.projection.resolveElement(render).parent = this;
-            this.placeChoice(render, container);
+        }else if(isConcept){
+            if(!this.model.hasProjectionSchema(value, template.tag)){
+                return container;
+            }
+
+            let choiceProjection = this.model.createProjection(value, template.tag).init({focusable: false});
+            choiceProjection.readonly = true;
+            choiceProjection.focusable = false;
+            choiceProjection.parent = this.projection;
+
+            container.append(choiceProjection.render());
         }else{
             container.append(value.toString);
         }
 
-        this.placeholders.push(container);
-
         return container;
     },
 
-    placeChoice(render, container){
-        let markerContainer = render.querySelector("[data-position]");
-
-        let x = Number(markerContainer.getAttribute("data-markerX"));
-        let y = Number(markerContainer.getAttribute("data-markerY"));
-
-        container.setAttribute("x", x);
-        container.setAttribute("y", y);
+    focusIn(){
+        console.warn(`FOCUSIN_HANDLER NOT IMPLEMENTED FOR ${this.name}`);
+        return;
     },
 
-    setValue(value, update = false){
-        var response = null;
+    focusOut(){
+        console.warn(`FOCUSOUT_HANDLER NOT IMPLEMENTED FOR ${this.name}`);
+        return;
+    },
 
-        if(update){
-            response = this.source.setValue(value);
+    focus() {
+        return;
+    },
 
-            if(!response.success){
-                this.environment.notify(response.message, NotificationType.ERROR);
+    setValue(value) {
+        let response = this.source.setValue(value);
+
+        if(!response.success){
+            this.environment.notify(response.message, NotificationType.ERROR);
+        }
+
+        if(!isNullOrUndefined(this.schema.choice.link)) {
+            let index = this.projection.findView(this.schema.choice.link);
+            
+            if(index == -1) {
+                return false;
             }
+
+            this.projection.changeView(index)
 
             return true;
         }
+    },
 
-        if(isNullOrUndefined(value)){
-            /**To do: cancel selection */
-        } else if (value.type === "meta-concept"){
-            this.value = value.name;
-        } else {
-            this.value = value;
+    clickHandler(target) {
+        const item = getItem.call(this, target);
+
+        if(!isNullOrUndefined(item)){
+            this.setValue(getItemValue.call(this, item));
         }
+
+        return false;
+    },
+
+    updateSize() {
+        if(this.fixed) {
+            return;
+        }
+
+        this.fixed = true;
+
+        let item = this.element.childNodes[0].childNodes[0];
+        let itemProjection = this.projection.resolveElement(item);
+
+        let minX, minY, maxX, maxY;
+
+        if(!isNullOrUndefined(itemProjection.containerView)) {
+            const {targetX, targetY, targetW, targetH } = itemProjection.containerView;
+
+            minX = targetX;
+            minY = targetY;
+            maxX = targetX + targetW;
+            maxY = targetY + targetH;
+
+        } else if (hasDimensions(item)) {
+            minX = Number(item.getAttribute("x"));
+            minY = Number(item.getAttribute("y"));
+            maxX = minX + Number(item.getAttribute("width"));
+            maxY = minY + Number(item.getAttribute("height"));
+        } else {
+            let box = item.getBBox();
+
+            minX = box.x;
+            minY = box.y;
+            maxX = box.x + box.width;
+            maxY = box.y + box.height;
+        }
+
+        this.element.childNodes.forEach((node) => {
+            item = node.childNodes[0];
+            itemProjection = this.projection.resolveElement(item);
+
+            if(!isNullOrUndefined(itemProjection.containerView)) {
+                const {targetX, targetY, targetW, targetH } = itemProjection.containerView;
+    
+                minX = Math.min(minX, targetX);
+                minY = Math.min(minY, targetY);
+                maxX = Math.max(maxX, targetX + targetW);
+                maxY = Math.max(maxY, targetY + targetH);
+    
+            } else if (hasDimensions(item)) {
+                minX = Math.min(minX, Number(item.getAttribute("x")));
+                minY = Math.min(minY, Number(item.getAttribute("y")));
+                maxX = Math.max(maxX, Number(item.getAttribute("x")) + Number(item.getAttribute("width")));
+                maxY = Math.max(maxY, Number(item.getAttribute("y")) + Number(item.getAttribute("height")));
+            } else {
+                let box = item.getBBox();
+    
+                minX = Math.min(minX, box.x);
+                minY = Math.min(minY, box.y);
+                maxX = Math.max(maxX, box.x + box.width);
+                maxY = Math.max(maxY, box.y + box.height);
+            }
+        })
+
+        this.element.setAttribute("width", maxX - Math.min(minX, 0));
+        this.element.setAttribute("height", maxY - Math.min(minY, 0));
+    },
+
+    display() {
+        if(!this.parent.displayed || this.displayed) {
+            return;
+        }
+
+        this.displayed = true;
+
+        this.element.childNodes.forEach( (node) => {
+            let projection = this.projection.resolveElement(node.childNodes[0]);
+            projection.projection.update("displayed");
+        })
 
         this.updateSize();
     },
 
-    updateSize(){
-        this.adaptView();
-        this.parent.updateSize();
-    },
-
-    adaptView(){
-        if(isNullOrUndefined(this.containerView)){
-            this.containerView = {}
-        }
-
-        let x = 0;
-        let y = 0;
-        let maxX = valOrDefault(Number(this.icon.getAttribute("width")), this.icon.getBBox().width);
-        let maxY = valOrDefault(Number(this.icon.getAttribute("height")), this.icon.getBBox().height);
-
-        if(this.closed){
-            this.containerView.targetW = maxX;
-            this.containerView.targetH = maxY;
-            return;
-        }
-        
-        let minX = x;
-        let minY = y;
-
-        this.placeholders.forEach(placeholder => {
-            let element = placeholder.childNodes[0];
-            let projection = this.projection.resolveElement(element);
-
-            let x = valOrDefault(Number(placeholder.getAttribute("x"), element.getBBox().x));
-            let y = valOrDefault(Number(placeholder.getAttribute("y"), element.getBBox().y));
-
-            minX = Math.min(x, minX);
-            minY = Math.min(y, minY);
-
-            if(!isNullOrUndefined(projection) && !isNullOrUndefined(projection.containerView)){
-                maxX = x + projection.containerView.targetW;
-                maxY = y + projection.containerView.targetH;
-            }else{
-                maxX = x + valOrDefault(Number(element.getAttribute("width")), element.getBBox().width);
-                maxY = y + valOrDefault(Number(element.getAttribute("height")), element.getBBox().height);
-            }
-
-        })
-
-        this.containerView.targetW = maxX - minX;
-        this.containerView.targetH = maxY - minY;
-
-        return;
-    },
-
-    focusIn(){
-        if(this.closeable){
-            this.choices.classList.remove("hidden");
-            this.choices.dataset.state = "open";
-            this.closed = false;
-        }
-
-        this.adaptView();
-
-        return this;
-    },
-
-    focusOut(){
-
-    },
-
-    clickHandler(target){
-        const item = getItem.call(this, target);
-
-        if(!isNullOrUndefined(item) && target !== this.selection){
-
-            this.setValue(getItemValue.call(this, item), true);
-
-            this.selectList.classList.add("hidden");
-            this.selectList.dataset.state = "close";
-            this.closed = true;
-            this.updateSize();
-        }else if (this.icon.contains(target)){
-            this.selectList.classList.remove("hidden");
-            this.selectList.dataset.state = "open";
-            this.closed = false;
-            this.source.removeValue();
-            this.updateSize();
-        }
-    },
-
-    bindEvents(){
+    bindEvents() {
         this.projection.registerHandler("displayed", () => {
-            if(!this.parent.displayed){
-                return;
-            }
-            if(this.displayed){
-                return;
-            }
-            this.displayed = true;
-
-            if(this.parent.displayed){
-                this.placeholders.forEach((element) => {
-                    this.projection.resolveElement(element.childNodes[0]).projection.update("displayed");
-                })
-            }
-
-            this.closed = true;
-            this.selectList.classList.add("hidden");
-            this.selectList.dataset.state = "close";
-
-            this.updateSize();
+            this.display()
         })
     }
+    
+
 }
 
 export const PlaceholderField = Object.assign({},
